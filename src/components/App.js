667db0ed71961c5d63282c3be977ab42b837f2dd
Caroline LS <carolinelds@gmail.com>
2022-03-25 07:59:59 -0300

    Adiciona componente para tela de inicio

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import "./../css/reset.css";
 import "./../css/styles.css";
+import TelaInicio from "./TelaInicio";
+import TelaCards from "./TelaCards";
 
 export default function App(){
     return (
-        <>
-        </>
+        <div class="App">
+            <TelaInicio />
+            <TelaCards />
+        </div>
     );
 }
---
+++ src/components/TelaCards.js
@@ -0,0 +1,7 @@
+export default function TelaCards(){
+    return (
+        <div class="TelaCards">
+            <p>TESTE</p>
+        </div>
+    );
+}
